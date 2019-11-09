a147564ed35a0cede8abfa00f155c18cd62147ce
MinusGix <MinusGix@gmail.com>
2019-11-08 18:45:59 -0600

    Add types header file

---
+++ src/types.hpp
@@ -0,0 +1,34 @@
+#ifndef FILE_SEEN_TYPES
+#define FILE_SEEN_TYPES
+
+#include <cstdint>
+#include <variant>
+
+enum class Register {
+    Alpha=0,
+    Beta=1
+};
+using Immediate = uint32_t;
+
+// nand %alpha %beta
+struct NandRR {
+    Register to;
+    Register from;
+    explicit NandRR (Register t_to, Register t_from) : to(t_to), from(t_from) {}
+};
+// nand %alpha 0x5
+struct NandRI {
+    Register to;
+    Immediate num;
+    explicit NandRI (Register t_to, Immediate t_num) : to(t_to), num(t_num) {}
+};
+
+struct ResetR {
+    Register victim;
+    explicit ResetR (Register t_victim) : victim(t_victim) {}
+};
+
+
+using Instruction = std::variant<NandRR, NandRI, ResetR>;
+
+#endif

f0b6373a4f88b3d9b0441ba4f0af1daafef119a6
MinusGix <MinusGix@gmail.com>
2019-11-08 19:29:40 -0600

    Make instructions have sizes

--- src/types.hpp
+++ src/types.hpp
@@ -4,6 +4,10 @@
 #include <cstdint>
 #include <variant>
 
+const static size_t RegisterSize = 1;
+const static size_t ImmediateSize = 4;
+const static size_t InstructionSize = 1;
+
 // Same bytecode
 enum class Register {
     Alpha=0,
@@ -13,18 +17,24 @@ using Immediate = uint32_t;
 
 // nand %alpha %beta
 struct NandRR {
+    const static size_t size = InstructionSize + RegisterSize + RegisterSize;
+
     Register to;
     Register from;
     explicit NandRR (Register t_to, Register t_from) : to(t_to), from(t_from) {}
 };
 // nand %alpha 0x5
 struct NandRI {
+    const static size_t size = InstructionSize + RegisterSize + ImmediateSize;
+
     Register to;
     Immediate num;
     explicit NandRI (Register t_to, Immediate t_num) : to(t_to), num(t_num) {}
 };
 
 struct ResetR {
+    const static size_t size = InstructionSize + RegisterSize;
+
     Register victim;
     explicit ResetR (Register t_victim) : victim(t_victim) {}
 };
